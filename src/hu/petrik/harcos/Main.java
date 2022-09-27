5d86f8ebb9bd8717052b362604fb0fe06c3f5670
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-09-27 15:44:48 +0200

    Add menu system to game

--- src/hu/petrik/harcos/Main.java
+++ src/hu/petrik/harcos/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -28,5 +29,23 @@ public class Main {
             System.out.println(hList.get(i).toString());
         }
 
+        System.out.println("A Játék kezdete________________________");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Adja meg a karaktere nevét: ");
+        String name = sc.nextLine();
+        System.out.print("Adja meg melyik karakter sablont szeretné használni(1)");
+        int template = sc.nextInt();
+
+        Harcos player = new Harcos(name, template);
+
+        String option = null;
+        while (!option.equals("q")){
+            for (int i = 0; i < hList.size(); i++) {
+                System.out.println(i+1+" - "+hList.get(i).toString());
+            }
+            System.out.println("Válassz egy opciót\n\tCsata egy harcossal (1)\n\tGyógyulás (2)\n\tKilépés (q)");
+            option=sc.nextLine().toLowerCase();
+
+        }
     }
 }
